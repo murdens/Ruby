0131638dd998480650c4402e9d485ac3c5f569e7
murdens <sonya.murden@gmail.com>
2020-02-19 23:30:11 +0000

    update leapyear

--- leapyear.rb
+++ leapyear.rb
@@ -1,10 +1,13 @@
-#years = []
-puts 'input year'
-year = gets.chomp
-#puts 'input end year'
-#last = gets.chomp + 1
-#years = [first..last]
-if (year % 100 !=0 or year % 4 == 0 or year % 400 == 0)
-    puts year.to_s + ' is a leapyear'
-else year.to_s + 'is not a leapyear'
+puts 'input start year'
+start_year = gets.chomp
+puts 'input end year'
+end_year = gets.chomp
+puts ''
+while start_year.to_i <= end_year.to_i
+    if (start_year%100!=0 and start_year%4 == 0 or start_year%400 == 0)
+    puts start_year.to_s + ' is a leapyear'
+    else 
+    puts start_year.to_s + 'is not a leapyear'
+    end
+    start_year = start_year.to_i + 1
 end
